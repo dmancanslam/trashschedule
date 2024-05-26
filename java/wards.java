01bf0eaee9c79d667e251a74ee6e64e679fd9f71
dmancanslam <lordelod@kean.edu>
2024-05-25 22:31:21 -0400

    First commit for git, wards.java works for returning a ward numer OF TYPE STRING.

---
+++ java/wards.java
@@ -0,0 +1,90 @@
+package trashschedule;
+
+import java.util.Scanner;
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class wards {
+	private static Scanner input;
+	
+	public static boolean compareIgnoringSpaces(String str1, String str2) {
+        // Remove all whitespace characters from both strings
+        String cleanedStr1 = str1.replaceAll("\\s", "");
+        String cleanedStr2 = str2.replaceAll("\\s", "");
+
+        // Compare the cleaned strings (case-insensitive)
+        return cleanedStr1.equalsIgnoreCase(cleanedStr2);
+    }
+
+	public static String search(String[] address) {
+		String ward = "Ward not found";
+		boolean evenAddr = (Integer.parseInt(address[0]) % 2==0); // quick cast to int to check for E or O in csv file
+		String EorO;
+		if(evenAddr) {
+			EorO = "E";
+		}
+		else {
+			EorO = "O";
+		}
+		
+		try {
+			File myObj = new File("./countystreetlist04032017.csv");
+			Scanner myReader = new Scanner(myObj);
+			
+			while (myReader.hasNextLine()) {
+				String line = myReader.nextLine();
+				String data[] = line.split(",");
+				if (!(EorO.equals(data[3]))) { // skip csv read if not even or odd
+					continue;					// reduces some time to iterate through csv
+				}
+				if (compareIgnoringSpaces(data[0], address[1])) {																	// below line checks lower and upper bounds
+					if (Integer.parseInt(data[1]) <= Integer.parseInt(address[0]) && Integer.parseInt(data[2]) >= Integer.parseInt(address[0])) { // SUPER lazy way to parse ints. probably should make a function to make the nextline() thing into an array instead of leaving it as 1 big string per line.
+						if (data[4].equals(address[2])) { // super strict way to check if input of zip code is identical to csv file's zip
+							return data[5];
+						}
+					}
+				}
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return ward;
+	}
+
+	public static void main(String[] args) {
+
+		String address_number;
+		String address_name = "";
+		String zip_code;
+		input = new Scanner(System.in);
+		System.out.print("Enter your address in this format: ### ADDRESS NAME ##### ");
+		while (true) {
+
+			String UserAddressRaw = input.nextLine(); // Read user input
+			String[] pieces = UserAddressRaw.split(" ");
+			for (int i = 0; i < pieces.length; i++) {
+				if (i != 0 && i != pieces.length - 1) {
+
+					address_name += pieces[i]+ " ";
+
+				}
+
+			}
+			address_number = pieces[0];
+			zip_code = pieces[pieces.length - 1];
+
+			System.out.println("Address Number: " + address_number);
+			System.out.println("Address Name: " + address_name);
+			System.out.println("Zipcode: " + zip_code);
+			
+			String[] prepped_addr = {address_number, address_name, zip_code};
+			System.out.println("function returns: " + search(prepped_addr));
+
+			break;
+
+		}
+
+	}
+}
