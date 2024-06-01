3870986b992f2fa7a3cfade786fd878f486242ab
dmancanslam <lordelod@kean.edu>
2024-06-01 10:13:47 -0400

    first commit from eclipse IDE

---
+++ trashschedule/src/trashschedule/main.java
@@ -0,0 +1,27 @@
+package trashschedule;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) { // main driver code to 
+		Scanner input = new Scanner(System.in); // for java console
+		System.out.print("Enter your address in this format: ### ADDRESS NAME ##### ");
+			String UserAddressRaw = input.nextLine(); // Read user input
+
+			int wardNum = Integer.parseInt(wards.search(parse.cleanupInput(UserAddressRaw)));
+			
+			display.writeTable(wardNum); // static cast to java file located in same directory. Might need to change this to a 'display' object and call the method that way.
+			
+			if (wardNum <4 && wardNum>0){ // wards 1-3 have the same schedule
+				System.out.println("This table shows an X when the trash will be picked up in the morning. \nKindly place your trash at the curb the previous day after 8:00pm.");
+			}
+			else if (wardNum >3 && wardNum<7){ // wards 4-6 have the same schedule
+				System.out.println("This table shows an X when the trash will be picked up in the morning. \nKindly place your trash at the curb the previous day after 8:00pm.");
+			}
+			else {
+				System.out.print("Error in IF statement for displaying additional info");
+			}
+	}
+
+}
