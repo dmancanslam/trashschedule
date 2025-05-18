fdf5a753b8aec7574628bc6b01630cc80467a0fa
dmancanslam <122804062+dmancanslam@users.noreply.github.com>
2025-05-18 19:56:18 -0400

    Updated Wards comments a bit

--- trashschedule/src/elizabeth/Wards.java
+++ trashschedule/src/elizabeth/Wards.java
@@ -11,7 +11,8 @@ public class Wards {
 
 	public static String search(String[] address) {
 		String ward = "Ward not found. Input address parsed: "+address[1];
-		boolean evenAddr = (Integer.parseInt(address[0]) % 2==0); // quick cast to int to check for E or O in csv file
+		// Quick cast to int to check for E or O in csv file
+		boolean evenAddr = (Integer.parseInt(address[0]) % 2==0);
 		String EorO;
 		if(evenAddr) {
 			EorO = "E";
@@ -27,12 +28,14 @@ public class Wards {
 			while (myReader.hasNextLine()) {
 				String line = myReader.nextLine();
 				String data[] = line.split(",");
-				if (!(EorO.equals(data[3]))) { // skip csv read if not even or odd
-					continue;					// reduces some time to iterate through csv
+				// Skip csv read if the (ward in the county / street side in the county) is not even or odd
+				// Reduces time to iterate through csv because this iterates linerally
+				if (!(EorO.equals(data[3]))) {
+					continue;				
 				}
 				if (Parse.compareIgnoringSpaces(data[0], address[1])) {																	// below line checks lower and upper bounds
 					if (Integer.parseInt(data[1]) <= Integer.parseInt(address[0]) && Integer.parseInt(data[2]) >= Integer.parseInt(address[0])) { // SUPER lazy way to parse ints. probably should make a function to make the nextline() thing into an array instead of leaving it as 1 big string per line.
-						if (data[4].equals(address[2])) { // super strict way to check if input of zip code is identical to csv file's zip
+						if (data[4].equals(address[2])) { // Static way to check if input of zip code is identical to csv file's zip
 							return data[5];
 						}
 					}
